64d88b356e7e53d7ffedd23a89a33b9fb8eeba44
Nate Beaty <nate@firebellydesign.com>
2017-11-14 12:32:22 -0600

    add header to taxonomy pages (workshop series) with clear button, style to match design

--- web/app/themes/cfs/taxonomy-workshop_series.php
+++ web/app/themes/cfs/taxonomy-workshop_series.php
@@ -1,6 +1,22 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
+$taxonomy = get_queried_object();
+get_template_part('templates/page', 'header');
+?>
 
 <div class="column-wrap"><div class="color-wrap">
+
+  <div class="taxonomy-header">
+    <div class="grid">
+      <div class="taxonomy-title grid-item">
+        <h3>Showing <?= $taxonomy->name ?> workshops</h3>
+      </div>
+      <div class="taxonomy-clear grid-item">
+        <h3>
+          <a href="/workshops/"><span>Show All Workshops</span> <svg class="icon icon-x" aria-hidden="hidden" role="image"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-x"></use></svg></a>
+        </h3>
+      </div>
+    </div>
+  </div>
 
   <div class="article-list"><div class="grid">
   <?php while (have_posts()) : the_post(); ?>
