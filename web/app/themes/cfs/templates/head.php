c46726c8c0e7808657f66b4725fcbd19a8889359
Nate Beaty <nate@firebellydesign.com>
2017-11-27 15:36:49 -0600

    fix pagination background color on mobile, pull bold + bold-italic styles from google fonts

--- web/app/themes/cfs/templates/head.php
+++ web/app/themes/cfs/templates/head.php
@@ -3,7 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <link href="https://fonts.googleapis.com/css?family=Anton|Rubik+Mono+One|Rubik:400,400i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Anton|Rubik+Mono+One|Rubik:400,400i,700,700i" rel="stylesheet">
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
