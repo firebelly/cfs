ab1d4209e6b59be2e2434853a3fcdf2e174c5425
Nate Beaty <nate@firebellydesign.com>
2017-11-27 17:43:07 -0600

    don't show error styles on nav search and footer newsletter form

--- web/app/themes/cfs/assets/scripts/main.js
+++ web/app/themes/cfs/assets/scripts/main.js
@@ -213,10 +213,12 @@ var CFS = (function($) {
 
     // Add .has-touched for styling errors (otherwise :invalid shows error styling before form is interacted with)
     $('form input[required]').one('blur keydown', function() {
-      $(this).addClass('has-touched').parent('form').addClass('has-touched');
+      if (!$(this).hasClass('no-error-styles')) {
+        $(this).addClass('has-touched').parent('form').addClass('has-touched');
+      }
     });
     $('form [type=submit]').on('click', function() {
-      $(this).parent('form').find('input[required]').addClass('has-touched');
+      $(this).parent('form').find('input[required]:not(.no-error-styles)').addClass('has-touched');
     });
   }
   function _checkFormInput(e) {
