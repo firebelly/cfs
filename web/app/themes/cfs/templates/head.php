4b757a67d6a1290b05c80063218933106bf78c17
Nate Beaty <nate@firebellydesign.com>
2017-12-18 14:06:35 -0600

    update analytics to new ID

--- web/app/themes/cfs/templates/head.php
+++ web/app/themes/cfs/templates/head.php
@@ -4,13 +4,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
   <link href="https://fonts.googleapis.com/css?family=Anton|Rubik+Mono+One|Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111413240-1"></script>
   <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-    ga('create', 'UA-107067865-1', 'auto');
-    ga('send', 'pageview');
+    gtag('config', 'UA-111413240-1');
   </script>
 </head>
