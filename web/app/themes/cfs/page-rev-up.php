bf5141191f1ae92acaf5d9a7758dd91d75fa7090
Nate Beaty <nate@firebellydesign.com>
2018-01-22 13:09:27 -0600

    add template name to rev up redirect template so it can be assigned

--- web/app/themes/cfs/page-rev-up.php
+++ web/app/themes/cfs/page-rev-up.php
@@ -1,6 +1,10 @@
 <?php
+/*
+  Template name: Rev Up redirect
+*/
+
 // Redirect to most recent Rev Up workshop (or homepage if none is available)
-$revup_post = \Firebelly\PostTypes\Workshop\get_workshop_like_title('Rev Up2');
+$revup_post = \Firebelly\PostTypes\Workshop\get_workshop_like_title('Rev Up');
 if ($revup_post) {
   $permalink = get_the_permalink($revup_post);
 } else {
