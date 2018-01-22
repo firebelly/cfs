effd51bb901ed55fe18301a8f4ebd04aba5ae7d8
Nate Beaty <nate@firebellydesign.com>
2018-01-22 14:11:53 -0600

    redirect single workshop view to /support-us/moments-of-justice/ if title matches "moments of justice"

--- web/app/themes/cfs/single-workshop.php
+++ web/app/themes/cfs/single-workshop.php
@@ -1,4 +1,10 @@
 <?php while (have_posts()) : the_post(); ?>
+	<?php
+	// Redirect to /support-us/moments-of-justice/ if title matches "moments of justice"
+	if (preg_match('/moments of justice/i', $post->post_title)) {
+		wp_redirect('/support-us/moments-of-justice/');
+	}
+	?>
 	<?php get_template_part('templates/page', 'header'); ?>
 	<?= \Firebelly\Fields\Posts\get_post_slideshow($post, ['treated_images' => true]); ?>
 <?php endwhile; ?>
