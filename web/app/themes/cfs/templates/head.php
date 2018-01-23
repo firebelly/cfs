e0dbbfa44f52e91e55f5fbc6a93178d27a9a509a
Nate Beaty <nate@clixel.com>
2018-01-23 13:13:33 -0600

    add favicon

--- web/app/themes/cfs/templates/head.php
+++ web/app/themes/cfs/templates/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png') ?>" />
   <?php wp_head(); ?>
   <link href="https://fonts.googleapis.com/css?family=Anton|Rubik+Mono+One|Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
   <!-- Global site tag (gtag.js) - Google Analytics -->
